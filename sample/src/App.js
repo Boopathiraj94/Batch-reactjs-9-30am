24cb3df617e332410c62a3d0e3e8f811c1b836ed
Boopathiraj94 <boopathirajk55@gmail.com>
2025-09-24 10:31:30 +0530

    basics

---
+++ sample/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <nav>
+     <div className='logo'>
+        <img src='https://c8.alamy.com/comp/2H6CXWT/modern-kb-logo-design-for-business-and-company-identity-creative-kb-letter-with-luxury-concept-2H6CXWT.jpg' alt='dsf' />
+     </div>
+     <div className='menubar'>
+        <a href='#'>Home</a>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+        <a href='#'>Services</a>
+     </div>
+     </nav>
+    </>
+
+    // <> .. </> fragment tag
+  );
+}
+
+// jsx => javascript xml
+
+export default App;
